f23ef2a9827b5ce1f387bd6b2948a35f6fbd5dbf
Michael Behrens <102385787+mbehrens-games@users.noreply.github.com>
2026-08-19 12:30:33 -0700

    Add files via upload

---
+++ octave/apu_env.m
@@ -0,0 +1,199 @@
+clear;
+clc;
+
+% envelope clock: 16 khz
+
+% envelope rates: 16 blocks, 8 rates per block (128 total)
+% starting envelope period is 1
+% starting envelope pattern step is 1
+% if the block is less than the base, period is left shifted by the difference
+% if the block is greater than the base, step is left shifted by the difference
+env_rate_base_block = 11;
+
+% envelope pattern tables
+env_patterns_ints = [ 0x0000, 0x0080, 0x0808, 0x0888, ...
+                      0x2222, 0x22A2, 0x2A2A, 0x2AAA, ...
+                      0x5555, 0x55D5, 0x5D5D, 0x5DDD, ...
+                      0x7777, 0x77F7, 0x7F7F, 0x7FFF];
+
+%{
+for m = 1:8
+  for n = 1:16
+    env_patterns_normal(16 * (m - 1) + n) = ...
+      bitget(env_patterns_ints(m + 8), 17 - n);
+  endfor
+endfor
+
+for m = 1:8
+  for n = 1:16
+    env_patterns_fast(16 * (m - 1) + n) = ...
+      1 + bitget(env_patterns_ints(2 * m - 1), 17 - n);
+  endfor
+endfor
+%}
+
+% envelope param time to rate mapping (100 values)
+env_rise_time_map = round(16 + 112 * (99:-1:0)/100);
+env_fall_time_map = round( 0 + 112 * (99:-1:0)/100);
+
+% envelope levels: 10 bit attenuation (0 to 1023)
+
+% envelope param sustain level mapping (100 values)
+env_sustain_level_map = [1023, round(512 * (99:-1:1)/99)];
+
+% envelope slope adjustments (for decay & sustain stages) (not needed?)
+env_decay_slopes = round(1024 * (env_sustain_level_map / 1023));
+env_sustain_slopes = round(1024 * ((1023 - env_sustain_level_map) / 1023));
+
+% print out tables and constants
+printf("Envelope Rate Base Block: \n");
+printf("#define APU_ENV_RATE_BASE_BLOCK %d\n", env_rate_base_block)
+printf("\n")
+
+printf("Envelope Step Patterns (as 16 Bit Ints) Table: \n");
+for m = 1:2
+  if (m == 1)
+    printf("  { ")
+  else
+    printf("    ")
+  endif
+  for n = 1:8
+    printf("0x%04X", env_patterns_ints(8 * (m - 1) + n))
+    if ((m < 2) || (n < 8))
+      printf(", ")
+    endif
+  endfor
+  printf("\n")
+endfor
+printf("  };")
+printf("\n\n")
+
+%{
+printf("Envelope Step Patterns (Normal) Table: \n");
+for m = 1:8
+  if (m == 1)
+    printf("  { ")
+  else
+    printf("    ")
+  endif
+  for n = 1:16
+    printf("%d", env_patterns_normal(16 * (m - 1) + n))
+    if ((m < 8) || (n < 16))
+      printf(", ")
+    endif
+  endfor
+  printf("\n")
+endfor
+printf("  };")
+printf("\n\n")
+
+printf("Envelope Step Patterns (Fast) Table: \n");
+for m = 1:8
+  if (m == 1)
+    printf("  { ")
+  else
+    printf("    ")
+  endif
+  for n = 1:16
+    printf("%d", env_patterns_fast(16 * (m - 1) + n))
+    if ((m < 8) || (n < 16))
+      printf(", ")
+    endif
+  endfor
+  printf("\n")
+endfor
+printf("  };")
+printf("\n\n")
+%}
+
+printf("Envelope Rise Time Map: \n");
+for m = 1:10
+  if (m == 1)
+    printf("  { ")
+  else
+    printf("    ")
+  endif
+  for n = 1:10
+    printf("%3d", env_rise_time_map(10 * (m - 1) + n))
+    if ((m < 10) || (n < 10))
+      printf(", ")
+    endif
+  endfor
+  printf("\n")
+endfor
+printf("  };")
+printf("\n\n")
+
+printf("Envelope Fall Time Map: \n");
+for m = 1:10
+  if (m == 1)
+    printf("  { ")
+  else
+    printf("    ")
+  endif
+  for n = 1:10
+    printf("%3d", env_fall_time_map(10 * (m - 1) + n))
+    if ((m < 10) || (n < 10))
+      printf(", ")
+    endif
+  endfor
+  printf("\n")
+endfor
+printf("  };")
+printf("\n\n")
+
+printf("Envelope Sustain Level Map: \n");
+for m = 1:10
+  if (m == 1)
+    printf("  { ")
+  else
+    printf("    ")
+  endif
+  for n = 1:10
+    printf("%4d", env_sustain_level_map(10 * (m - 1) + n))
+    if ((m < 10) || (n < 10))
+      printf(", ")
+    endif
+  endfor
+  printf("\n")
+endfor
+printf("  };")
+printf("\n\n")
+
+printf("Envelope Decay Slope Increments Table: \n");
+for m = 1:10
+  if (m == 1)
+    printf("  { ")
+  else
+    printf("    ")
+  endif
+  for n = 1:10
+    printf("%4d", env_decay_slopes(10 * (m - 1) + n))
+    if ((m < 10) || (n < 10))
+      printf(", ")
+    endif
+  endfor
+  printf("\n")
+endfor
+printf("  };")
+printf("\n\n")
+
+printf("Envelope Sustain Slope Increments Table: \n");
+for m = 1:10
+  if (m == 1)
+    printf("  { ")
+  else
+    printf("    ")
+  endif
+  for n = 1:10
+    printf("%4d", env_sustain_slopes(10 * (m - 1) + n))
+    if ((m < 10) || (n < 10))
+      printf(", ")
+    endif
+  endfor
+  printf("\n")
+endfor
+printf("  };")
+printf("\n\n")
+%}
+
---
+++ octave/apu_osc.m
@@ -0,0 +1,125 @@
+clear;
+clc;
+
+% oscillator clock: 48 khz
+
+% oscillator pitch table
+% base pitch is C-2
+% the table stores 48 values (1 octave in 1/4 semitone increments)
+% this is inspired by the YM 2151 table
+osc_pitch_base_block = 2;
+
+osc_pitch_base_f = 440 * exp(log(2) * (-2 - 9/12));
+osc_pitch_base_inc = osc_pitch_base_f * exp(log(2) * 20) / 48000;
+
+osc_pitch_table = round(osc_pitch_base_inc * exp(log(2) * (0:47)/48));
+
+% oscillator pitch deltas
+% 16 steps between quarter semitones (values in pitch table)
+% (delta * step) / 16 gets the change from the pitch table value for this step
+for m = 1:47
+  osc_pitch_deltas(m) = osc_pitch_table(m + 1) - osc_pitch_table(m);
+endfor
+
+osc_pitch_deltas(48) = (2 * osc_pitch_table(1)) - osc_pitch_table(48);
+
+% oscillator detune amounts
+% these are steps from the value in the pitch table
+% as above, (delta * step) / 16 gets the change from the pitch table value
+osc_pitch_detune = [0, 2, 4, 6];
+
+% oscillator sine wavetable
+osc_sine_table = round(-256 * (log(sin(2 * pi * (0.5+(0:255))/1024)) / log(2)));
+
+% oscillator level table (16 blocks, 256 entries per block)
+osc_level_table = round(32768 * exp(log(1/2) * (0:255)/256));
+
+% print out tables and constants
+
+printf("Oscillator Pitch Base Block: \n");
+printf("#define APU_OSC_PITCH_BASE_BLOCK %d\n", osc_pitch_base_block)
+printf("\n")
+
+printf("Oscillator Pitch Table: \n")
+for m = 1:12
+  if (m == 1)
+    printf("  { ")
+  else
+    printf("    ")
+  endif
+  for n = 1:4
+    printf("%4d", osc_pitch_table(4 * (m - 1) + n))
+    if ((m < 12) || (n < 4))
+      printf(", ")
+    endif
+  endfor
+  printf("\n")
+endfor
+printf("  };")
+printf("\n\n")
+
+printf("Oscillator Pitch Deltas: \n")
+for m = 1:12
+  if (m == 1)
+    printf("  { ")
+  else
+    printf("    ")
+  endif
+  for n = 1:4
+    printf("%2d", osc_pitch_deltas(4 * (m - 1) + n))
+    if ((m < 12) || (n < 4))
+      printf(", ")
+    endif
+  endfor
+  printf("\n")
+endfor
+printf("  };")
+printf("\n\n")
+
+printf("Oscillator Detune Table: \n")
+printf("  { ")
+for m = 1:4
+  printf("%d", osc_pitch_detune(m))
+  if (m < 4)
+    printf(", ")
+  endif
+endfor
+printf(" };")
+printf("\n\n")
+
+printf("Oscillator Sine Wavetable: \n")
+for m = 1:32
+  if (m == 1)
+    printf("  { ")
+  else
+    printf("    ")
+  endif
+  for n = 1:8
+    printf("%5d", osc_sine_table(8 * (m - 1) + n))
+    if ((m < 32) || (n < 8))
+      printf(", ")
+    endif
+  endfor
+  printf("\n")
+endfor
+printf("  };")
+printf("\n\n")
+
+printf("Oscillator Level Table: \n")
+for m = 1:32
+  if (m == 1)
+    printf("  { ")
+  else
+    printf("    ")
+  endif
+  for n = 1:8
+    printf("%5d", osc_level_table(8 * (m - 1) + n))
+    if ((m < 32) || (n < 8))
+      printf(", ")
+    endif
+  endfor
+  printf("\n")
+endfor
+printf("  };")
+printf("\n\n")
+
